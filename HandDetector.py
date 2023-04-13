b882c0495f8b9ccdb53b74f0cba26b2665e74032
Ali Abbas <aliabbasinayatali@gmail.com>
2023-04-13 22:48:20 +0500

    Basic Hand Detection Setup - First Commit

---
+++ HandDetector.py
@@ -0,0 +1,21 @@
+import cv2
+import mediapipe as mp
+
+mpHand = mp.solutions.hands
+hand = mpHand.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hand.process(imgRGB)
+    if  result.multi_hand_landmarks:
+        for handLM in result.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLM, mpHand.HAND_CONNECTIONS)
+            print(handLM)
+            handLM
+
+    cv2.imshow("Tracking", img)
+    cv2.waitKey(1)
